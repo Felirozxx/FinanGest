ab21ad839ac3a0a63693586501ee81b5fde59b5d
Felirozxx <fzuluaga548@gmail.com>
2026-01-10 03:25:15 -0300

    Add PWA: manifest, icons and service worker for installable app

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'finangest-v1';
+const urlsToCache = [
+  '/finangest.html',
+  '/manifest.json',
+  '/icon-192.svg',
+  '/icon-512.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
